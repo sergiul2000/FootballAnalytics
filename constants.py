6fb94af936029ad62363476a20306c758172df90
Dan Toderici <toderici_dan@yahoo.com>
2022-09-19 18:00:54 +0300

    who scored summary stat for each top club in top leagues crawler

--- constants.py
+++ constants.py
@@ -11,3 +11,56 @@ league_teams_dict = {'epl' : ["Manchester United", "Manchester City", "Chelsea",
            'ligue 1' : ["Paris Saint Germain", "Marseille", "Monaco", "Nice", "Rennes", "Lyon"],
            'serie a' : ["Juventus", "Inter", "AC Milan", "Napoli", "Roma", "Atalanta"]
            }
+
+
+whoscored_teams_dict = {
+           'epl' : 
+           {
+            "Manchester United" : 32, 
+            "Manchester City"   : 167, 
+            "Chelsea"           : 15,
+            "Liverpool"         : 26, 
+            "Arsenal"           : 13,
+            "Tottenham"         : 30,
+           },
+
+           'bundesliga' : 
+           {
+            "Bayern Munich"          : 37, 
+            "Borussia Dortmund"      : 44,
+            "Bayer Leverkusen"       : 36, 
+            "RasenBallsport Leipzig" : 7614,
+            "Eintracht Frankfurt"    : 45,
+            "Wolfsburg"              : 33,
+           },
+
+           'la liga' : 
+           {
+            "Real Madrid"       : 52, 
+            "Barcelona"         : 65, 
+            "Atletico Madrid"   : 63, 
+            "Sevilla"           : 67, 
+            "Valencia"          : 55, 
+            "Villarreal"        : 839,
+            },
+
+           'ligue 1' : 
+           {
+            "Paris Saint Germain" : 304, 
+            "Marseille"           : 249, 
+            "Monaco"              : 248, 
+            "Nice"                : 613, 
+            "Rennes"              : 313, 
+            "Lyon"                : 228,
+            },
+
+           'serie a' : 
+           {
+            "Juventus"  : 87, 
+            "Inter"     : 75, 
+            "AC Milan"  : 80, 
+            "Napoli"    : 276, 
+            "Roma"      : 84, 
+            "Atalanta"  : 300,
+           }
+}
--- who_scored_crawler.py
+++ who_scored_crawler.py
@@ -1,3 +1,6 @@
+from re import X
+from constants import whoscored_teams_dict
+
 import time
 import pandas as pd
 import time
@@ -6,6 +9,9 @@ from webdriver_manager.chrome import ChromeDriverManager
 from tqdm import tqdm
 from selenium import webdriver
 from selenium.webdriver.support.select import Select
+from empty_folder_creator import create_empty_directories
+
+
 
 def replace_pd(df):
     mapping = {'-': 0} 
@@ -16,27 +22,94 @@ def replace_pd(df):
         replace_dict[colum] = mapping
     return df.replace(replace_dict)
 
-def crawl_players_team_stats_for_available_seasons_by_competition(team_id, competition_name):
-    dict_of_league_urls = crawl_all_urls_for_given_team_in_league_competitions(team_id, competition_name)
 
+
+def save_stats_csv(df, stat, league, team, season_start_year):
+    print('Saving CSV to File')
+    team = team.replace(' ','_')
+    filename = f'dataframes/whoscored_player_{stat}_stats/{league}/season_{season_start_year}_{season_start_year+1}/{team}_{stat}_stats_in_season_{season_start_year}_{season_start_year+1}.csv'
+
+    print(filename)
+    df.to_csv(filename)
+
+
+def crawl_chosen_stats_between_years(league, teams_details, start_year, end_year, stat='summary'):
+
+    create_empty_directories('dataframes', f'whoscored_player_{stat}_stats', league, start_year, end_year, True)
+
+    for team, code in teams_details.items():
+
+       # print(team, '->', code)
+
+        dict_of_league_urls = crawl_all_urls_for_given_team_in_league_competitions(code, league, last_season_year= 2022)
+
+        for item in dict_of_league_urls.items():
+            print(item)
+            
+            match stat:
+                case 'summary':
+                    df_season_stats = crawl_player_team_stats_summary(item[1])
+                    
+                    save_stats_csv(df_season_stats,'summary',league, team, season_start_year = int(item[0].split('_')[1]))
+                    #df_season_stats = crawl_player_team_stats_summary(item[1])
+                    #df_season_stats = crawl_player_team_stats_summary(item[1])
+                    #df_season_stats.to_csv(f'test/Barcelona_season_{item[0]}.csv')
+                case 'offensive':
+                    print('2')
+                case 'defensive':
+                    print('3')
+                case _:
+                    print('nada')      
+
+
+
+
+def crawl_players_team_stats_for_available_seasons_by_competition(team_id, competition_name):
+
+    dict_of_league_urls = crawl_all_urls_for_given_team_in_league_competitions(team_id, competition_name)
 
     for item in dict_of_league_urls.items():
         print(item)
         df_season_stats = crawl_player_team_stats_summary(item[1])
+    
         df_season_stats.to_csv(f'test/Barcelona_season_{item[0]}.csv')
 
 
+def transform_competition_name(competition_name):
+    match competition_name:
+        case 'epl':
+            competition_text = 'Premier League'
+            season_text_start = 'epl_'
+        case 'bundesliga':
+            competition_text = 'Bundesliga'
+            season_text_start = 'bundesliga_'
+        case 'la liga':
+            competition_text = 'La Liga'
+            season_text_start = 'la liga_'
+        case 'ligue 1':
+            competition_text = 'Ligue 1'
+            season_text_start = 'ligue 1_'
+        case 'serie a':
+            competition_text = 'Serie A'
+            season_text_start = 'seria a_'
+        case _:
+            competition_text = 'La Liga'
+            season_text_start = 'la liga_'
 
-def crawl_all_urls_for_given_team_in_league_competitions(team_id, competition_name):
+    return competition_text,season_text_start
+
+
+def crawl_all_urls_for_given_team_in_league_competitions(team_id, competition_name, last_season_year):
     url_current_season = f'https://www.whoscored.com/Teams/{team_id}'
     url = f'https://www.whoscored.com/Teams/{team_id}/Archive'
     dict_of_league_competitions_urls = dict()
-    dict_of_league_competitions_urls[f'{competition_name}_2022_2023'] = url_current_season
+    dict_of_league_competitions_urls[f'{competition_name}_{last_season_year}_{last_season_year+1}'] = url_current_season
 
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     print('Installing Chrome driver')
     driver.get(url)
 
+    #print(url)
     dropDown = driver.find_element("id","stageId")
     select = Select(dropDown)
     for option in select.options:
@@ -44,23 +117,24 @@ def crawl_all_urls_for_given_team_in_league_competitions(team_id, competition_na
         #print(option.text)
         value = option.get_attribute('value')
 
+
+        competition_text,season_text_start = transform_competition_name(competition_name)
         
-        if competition_name in option.text:
+        if competition_text in option.text:
             season_full = option.text.split('-')[1][1:]
             start_year = season_full.split('/')[0]
             end_season = season_full.split('/')[1]
             season_text = f'{start_year}_{end_season}'
-            season_text = f'LaLiga_{season_text}'
-            print(season_text)
+            season_text = f'{season_text_start}{season_text}'
+            #print(season_text)
 
             url_to_append = f'https://www.whoscored.com/Teams/{team_id}/Archive/?stageId={value}'
             dict_of_league_competitions_urls[season_text] = url_to_append
 
-    print("DICT COMPETITIONS URLS")
-    print(dict_of_league_competitions_urls)
+   # print("DICT COMPETITIONS URLS:")
+    #print(dict_of_league_competitions_urls)
     return dict_of_league_competitions_urls
 
-       
 
 def crawl_player_team_stats_summary(url, api_delay_term=5):
     """
@@ -82,7 +156,7 @@ def crawl_player_team_stats_summary(url, api_delay_term=5):
     driver.get(url)
 
     # wait for getting data
-    print('Waiting for getting data')
+    print('Waiting to get data')
     time.sleep(api_delay_term)
     
     # make pandas dataframe
@@ -119,7 +193,7 @@ def crawl_player_team_stats_summary(url, api_delay_term=5):
             "rating": element.find_elements("css selector","td")[14].text,  
         }
 
-        print(f'Populating CSV with {player_dict}')
+        #print(f'Populating CSV with {player_dict}')
         player_summary_df.loc[len(player_summary_df)] = player_dict
        
     
@@ -144,6 +218,22 @@ def crawl_player_team_stats_summary(url, api_delay_term=5):
     
     return player_summary_df
 
+def crawl_player_team_stats_offensive(url, api_delay_term=5):
+    return 0
+
+def crawl_player_team_stats_defensive(url, api_delay_term=5):
+    return 0
+
+def crawl_player_team_stats_offensive(url, api_delay_term=5):
+    return 0
+
+def crawl_player_team_stats_passing(url, api_delay_term=5):
+    return 0
+
+def crawl_player_team_stats_detailed(url, api_delay_term=5):
+    return 0
+
+
 if __name__ == "__main__":
     #65 barcelona | 26 liverpool
     # Barcelona = crawl_player_team_stats_summary(65, api_delay_term=5)
@@ -152,4 +242,17 @@ if __name__ == "__main__":
     # Barcelona.to_csv("Barcelona.csv")
 
     #crawl_all_urls_for_given_team_in_league_competitions(65, "LaLiga")
-    crawl_players_team_stats_for_available_seasons_by_competition(65, "LaLiga")
+
+    for league, teams_details in whoscored_teams_dict.items():
+        print(league, '->', teams_details)
+        #crawl_chosen_stats_between_years(league, teams_details, 2009,2023)
+        crawl_chosen_stats_between_years(league, teams_details, 2009, 2023)
+        # for team, code in teams_details.items():
+        #     print(team, '->', code)
+    
+    
+
+
+    
+    #crawl_players_team_stats_for_available_seasons_by_competition(65, "LaLiga")
+
