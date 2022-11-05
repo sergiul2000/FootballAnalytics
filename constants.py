02935491cb733c7e536f601a69fa31b14a5d71a1
Dan Toderici <toderici_dan@yahoo.com>
2022-11-05 15:40:13 +0200

    bug fix in relative paths

--- constants.py
+++ constants.py
@@ -159,14 +159,14 @@ whoscored_detailed_options = {
 }
 
 # realative path
-whoscored_summary_relative_path = 'dataframes/player_summary_stats'
-whoscored_offensive_relative_path  = 'dataframes/player_offensive_stats'
-whoscored_defensive_relative_path  = 'dataframes/player_defensive_stats'
-whoscored_passing_relative_path  = 'dataframes/player_passing_stats'
+whoscored_summary_relative_path = 'dataframes/whoscored_stats/player_summary_stats'
+whoscored_offensive_relative_path  = 'dataframes/whoscored_stats/player_offensive_stats'
+whoscored_defensive_relative_path  = 'dataframes/whoscored_stats/player_defensive_stats'
+whoscored_passing_relative_path  = 'dataframes/whoscored_stats/player_passing_stats'
 
-understat_clean_sheets_relative_path  = 'dataframes/clean_sheets'
-understat_league_table_relative_path  = 'dataframes/league_table'
-understat_player_team_stats_relative_path  = 'dataframes/player_team_stats'
+understat_clean_sheets_relative_path  = 'dataframes/understat/clean_sheets'
+understat_league_table_relative_path  = 'dataframes/understat/league_table'
+understat_player_team_stats_relative_path  = 'dataframes/understat/player_team_stats'
 
 
 
